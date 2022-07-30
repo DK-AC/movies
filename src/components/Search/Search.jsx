fa83b85b6994a0604088a23b32402a77d2a3f025
DK-AC <denkacaj@gmail.com>
2022-07-30 17:56:48 +0300

    refactoring

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,31 +4,39 @@ import styles from './Search.module.css'
 
 export const Search = ({ onChangeMovieTitle }) => {
   const [movieTitle, setMovieTitle] = useState('')
+  const [error, setError] = useState('')
 
   const onSearchMoviesHandle = event => {
     setMovieTitle(event.currentTarget.value)
   }
 
-  const changeMovieTitleSearchClick = () => {
-    onChangeMovieTitle(movieTitle)
+  const searchMovieTitleClick = () => {
+    if (movieTitle.trim() === '') {
+      setError('Enter movie title')
+    } else {
+      onChangeMovieTitle(movieTitle)
+      setError(null)
+      setMovieTitle('')
+    }
   }
 
   return (
     <div className="row">
       <input
-        placeholder="search"
+        placeholder="Search"
         type="search"
-        className="validate"
+        className="center"
         value={movieTitle}
         onChange={onSearchMoviesHandle}
       />
       <button
         className={`btn ${styles.searchButton} blue darken-4`}
         type="button"
-        onClick={changeMovieTitleSearchClick}
+        onClick={searchMovieTitleClick}
       >
         search
       </button>
+      {error ? <div className={styles.error}>{error}</div> : ''}
     </div>
   )
 }
